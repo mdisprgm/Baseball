b0af1560172401dfde9e9e270d4885db993ce138
MDISPRGM <mdisprgm@naver.com>
2021-06-29 17:09:20 +0900

    NBResultArray, not completed

--- Baseball/NumberBaseballResultArray.cpp
+++ Baseball/NumberBaseballResultArray.cpp
@@ -1,4 +1,37 @@
 #pragma once
 
 #include "NumberBaseballResultArray.h"
+#include <cstdlib>
+#include <iostream>
+#include <string>
 
+inline unsigned int isThere(char value, char* arr, int size) {
+	unsigned int cnt = 0;
+	for (int i = 0; i < size; i++) {
+		if (arr[i] == value) {
+			++cnt;
+		}
+	}
+
+	return cnt;
+}
+
+inline bool isStrike(char value, int index, char* arr, int size) {
+	unsigned int cnt = isThere(value, arr, size);
+	if (size == 1) {
+		for (int i = 0; i < size; i++) {
+			if (arr[i] == value && i == index) {
+				return true;
+			}
+		}
+	}
+	else if (size != 0) {
+		return false;
+		std::cout << "Invalid size!! in function " << __FUNCTION__ << std::endl;
+		exit(1);
+	}
+}
+
+inline bool isBall(char value, int index, char* arr, int size) {
+	return !isStrike(value, index, arr, size);
+}
--- Baseball/NumberBaseballResultArray.h
+++ Baseball/NumberBaseballResultArray.h
@@ -3,10 +3,10 @@
 #ifndef __NBRESULTARRAY_H__
 #define __NBRESULTARRAY_H__
 
-inline unsigned int isThere(char, char*);//returns count
+inline unsigned int isThere(char, char*, int);//returns count
 
-inline bool isBall(char, char*);
+inline bool isBall(char, char*, int size);
 
-inline bool isStrike(char, char*);
+inline bool isStrike(char, char*, int);
 
 #endif
