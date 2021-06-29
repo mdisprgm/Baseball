9901570a286c1f319473b9ba59402f885508efa5
MDISPRGM <mdisprgm@naver.com>
2021-06-29 16:16:03 +0900

    all comments are english, Numberbaseball.h

--- Baseball/NumberBaseball.h
+++ Baseball/NumberBaseball.h
@@ -5,11 +5,11 @@
 #ifndef __NUMBERBASEBALL_H__
 #define __NUMBERBASEBALL_H__
 
-typedef class NumberBaseball {//안내를 위한 Server 상속
+typedef class NumberBaseball {
 	const std::string serverName;
 
-	char _A = 0, _B = 0, _C = 0; //49 == '1', 설정된 정답
-	char _Afp = 0, _Bfp = 0, _Cfp = 0; //fp == Got From Player, 플레이어가 예측한 정답
+	char _A = 0, _B = 0, _C = 0; //49 == '1', real answer
+	char _Afp = 0, _Bfp = 0, _Cfp = 0; //fp == Got From Player, answer player guessed
 
 	inline bool isAvailable(char);
 public:
