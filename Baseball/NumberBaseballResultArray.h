2be0b7e22f74032a662835445c6cec01d44f7127
MDISPRGM <mdisprgm@naver.com>
2021-06-29 16:47:24 +0900

    in morenumbers branch, declared in .h NBResultArray

---
+++ Baseball/NumberBaseballResultArray.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifndef __NBRESULTARRAY_H__
+#define __NBRESULTARRAY_H__
+
+inline unsigned int isThere(char, char*);//returns count
+
+inline bool isBall(char, char*);
+
+inline bool isStrike(char, char*);
+
+#endif
