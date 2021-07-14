92dd014f92c73d0dc3b7bbf3fdf8143c74d00338
MDISPRGM <mdisprgm@naver.com>
2021-07-14 11:33:54 +0900

    _Strike and _Ball are initialized now

--- Baseball/NumberBaseballResult.h
+++ Baseball/NumberBaseballResult.h
@@ -4,7 +4,7 @@
 #define  __NUMBERBASEBALLRESULT_H__
 typedef unsigned int count_t;
 typedef class NumberBaseballResult {//strike,ball
-char _Strike, _Ball;//strike,ball
+char _Strike = 0, _Ball = 0;//strike,ball
 public:
 	NumberBaseballResult();
 	NumberBaseballResult(count_t, count_t);//s,b,o
