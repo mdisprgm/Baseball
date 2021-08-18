6207a6fb2d318ced3917ff5772a31c6e925c811d
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2021-08-18 11:50:40 +0900

    replace to typedefed class name to originals

--- Baseball/NumberBaseballResult.cpp
+++ Baseball/NumberBaseballResult.cpp
@@ -7,32 +7,32 @@
 #include "NumberBaseballResult.h"
 
 //constructors
-NBResult::NumberBaseballResult() {//default constructor
+NumberBaseballResult::NumberBaseballResult() {//default constructor
 	_Strike = 0;
 	_Ball = 0;
 }
-NBResult::NumberBaseballResult(count_t s, count_t b) {//constructor to set
+NumberBaseballResult::NumberBaseballResult(count_t s, count_t b) {//constructor to set
 	_Strike = s;
 	_Ball = b;
 }
 
 //getters
-count_t NBResult::strike() const {
+count_t NumberBaseballResult::strike() const {
 	return _Strike;
 }
-count_t NBResult::ball() const {
+count_t NumberBaseballResult::ball() const {
 	return _Ball;
 }
 
 //is out
-bool NBResult::isOut() {
+bool NumberBaseballResult::isOut() {
 	if (_Strike == 0 && _Ball == 0) {
 		return true;
 	}
 	return false;
 }
 //is homerun (WIN)
-bool NBResult::isHomerun() {
+bool NumberBaseballResult::isHomerun() {
 	if (_Strike == 3 && _Ball == 0) {
 		return true;
 	}
